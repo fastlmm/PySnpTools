7d9b6aaef97076746357056c7560ba457cebe4d0
Carl Kadie <carlk@msn.com>
2024-04-30 08:32:02 -0700

    v 0.5.11

--- pysnptools/snpreader/dat.py
+++ pysnptools/snpreader/dat.py
@@ -8,17 +8,18 @@ from pysnptools.snpreader import SnpData
 import warnings
 from pysnptools.pstreader import _OneShot
 
-#LATER say that this is also called dosage or bimbam
-#LATER work when the extension is \*.dosage or anything else
-#LATER make so fam can be called bam and map can be called bim
-class Dat(_OneShot,SnpReader):
-    '''
-    A :class:`.SnpReader` for reading Dat/Fam/Map-formated files from disk.
+
+# LATER say that this is also called dosage or bimbam
+# LATER work when the extension is \*.dosage or anything else
+# LATER make so fam can be called bam and map can be called bim
+class Dat(_OneShot, SnpReader):
+    """
+    A :class:`.SnpReader` for reading Dat/Fam/Map-formatted files from disk.
 
     See :class:`.SnpReader` for general examples of using SnpReaders.
 
     This is a text format that can store any numeric values. (In contrast, Bed and Ped can only store 0,1,2, and missing). Its Dat files look like::
-    
+
         null_0  	j	n	0.333	1	2
         null_100	j	n	2	1	1
         null_200	j	n	0	nan	1
@@ -40,36 +41,65 @@ class Dat(_OneShot,SnpReader):
         (500, 10000)
 
     **Methods beyond** :class:`.SnpReader`
-    '''
+    """
 
     def __init__(self, filename, skiprows=0):
-        '''
+        """
         filename    : string of the name of the Dat file.
-        '''
+        """
         super(Dat, self).__init__()
-        self.filename = SnpReader._name_of_other_file(filename,remove_suffix="dat", add_suffix="dat")
+        self.filename = SnpReader._name_of_other_file(
+            filename, remove_suffix="dat", add_suffix="dat"
+        )
         self.skiprows = skiprows
 
     def _read_pstdata(self):
-        row = SnpReader._read_fam(self.filename,remove_suffix="dat")
-        col, col_property = SnpReader._read_map_or_bim(self.filename,remove_suffix="dat", add_suffix="map")
-        if len(row)==0 or len(col)==0:
-            return SnpData(iid=row,sid=col,pos=col_property,val=np.empty([len(row),len(col)]))
-        datfields = pd.read_csv(self.filename,delimiter = '\t',header=None,index_col=False,skiprows=self.skiprows)
-        if not np.array_equal(datfields[0], col) : raise Exception("Expect snp list in map file to exactly match snp list in dat file")
+        row = SnpReader._read_fam(self.filename, remove_suffix="dat")
+        col, col_property = SnpReader._read_map_or_bim(
+            self.filename, remove_suffix="dat", add_suffix="map"
+        )
+        if len(row) == 0 or len(col) == 0:
+            return SnpData(
+                iid=row, sid=col, pos=col_property, val=np.empty([len(row), len(col)])
+            )
+        datfields = pd.read_csv(
+            self.filename,
+            delimiter="\t",
+            header=None,
+            index_col=False,
+            skiprows=self.skiprows,
+        )
+        if not np.array_equal(datfields[0], col):
+            raise Exception(
+                "Expect snp list in map file to exactly match snp list in dat file"
+            )
         del datfields[0]
         del datfields[1]
         del datfields[2]
-        assert len(row) == datfields.shape[1], "Expect # iids in fam file to match dat file"
+        assert (
+            len(row) == datfields.shape[1]
+        ), "Expect # iids in fam file to match dat file"
         val = datfields.values.T
-        snpdata = SnpData(iid=row,sid=col,pos=col_property,val=val)
+        snpdata = SnpData(iid=row, sid=col, pos=col_property, val=val)
         return snpdata
 
     def copyinputs(self, copier):
         # doesn't need to self.run_once() because creates name of all files itself
-        copier.input(SnpReader._name_of_other_file(self.filename,remove_suffix="dat", add_suffix="dat"))
-        copier.input(SnpReader._name_of_other_file(self.filename,remove_suffix="dat", add_suffix="fam"))
-        copier.input(SnpReader._name_of_other_file(self.filename,remove_suffix="dat", add_suffix="map"))
+        copier.input(
+            SnpReader._name_of_other_file(
+                self.filename, remove_suffix="dat", add_suffix="dat"
+            )
+        )
+        copier.input(
+            SnpReader._name_of_other_file(
+                self.filename, remove_suffix="dat", add_suffix="fam"
+            )
+        )
+        copier.input(
+            SnpReader._name_of_other_file(
+                self.filename, remove_suffix="dat", add_suffix="map"
+            )
+        )
 
     @staticmethod
     def write(filename, snpdata):
@@ -91,27 +121,42 @@ class Dat(_OneShot,SnpReader):
         Dat('tempdir/toydata10.dat')
         """
 
-        if isinstance(filename,SnpData) and isinstance(snpdata,str): #For backwards compatibility, reverse inputs if necessary
-            warnings.warn("write statement should have filename before data to write", DeprecationWarning)
-            filename, snpdata = snpdata, filename 
+        if isinstance(filename, SnpData) and isinstance(
+            snpdata, str
+        ):  # For backwards compatibility, reverse inputs if necessary
+            warnings.warn(
+                "write statement should have filename before data to write",
+                DeprecationWarning,
+            )
+            filename, snpdata = snpdata, filename
 
         SnpReader._write_fam(snpdata, filename, remove_suffix="dat")
-        SnpReader._write_map_or_bim(snpdata, filename, remove_suffix="dat", add_suffix="map")
-        filename = SnpReader._name_of_other_file(filename,remove_suffix="dat", add_suffix="dat")
+        SnpReader._write_map_or_bim(
+            snpdata, filename, remove_suffix="dat", add_suffix="map"
+        )
+        filename = SnpReader._name_of_other_file(
+            filename, remove_suffix="dat", add_suffix="dat"
+        )
 
         snpsarray = snpdata.val
-        with open(filename,"w") as dat_filepointer:
+        with open(filename, "w") as dat_filepointer:
             for sid_index, sid in enumerate(snpdata.sid):
                 if sid_index % 1000 == 0:
-                    logging.info("Writing snp # {0} to file '{1}'".format(sid_index, filename))
-                dat_filepointer.write("%s\tj\tn\t"%sid) #use "j" and "n" as the major and minor allele
-                row = snpsarray[:,sid_index]
+                    logging.info(
+                        "Writing snp # {0} to file '{1}'".format(sid_index, filename)
+                    )
+                dat_filepointer.write(
+                    "%s\tj\tn\t" % sid
+                )  # use "j" and "n" as the major and minor allele
+                row = snpsarray[:, sid_index]
                 dat_filepointer.write("\t".join((str(i) for i in row)) + "\n")
         logging.info("Done writing " + filename)
         return Dat(filename)
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
 
     import doctest
+
     doctest.testmod(optionflags=doctest.ELLIPSIS)
