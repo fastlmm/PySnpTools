572cf415c1f917a5efee262b4d684de4fd4106b1
Carl Kadie <carlk@msn.com>
2024-10-28 19:32:41 -0700

    all ruff lints and all pytests pass

--- pysnptools/distreader/__init__.py
+++ pysnptools/distreader/__init__.py
@@ -3,12 +3,12 @@ Tools for reading and manipulating SNP distribution data. For each individual an
 where A and B are the alleles. The probabilities should sum to 1.0. Missing data is represented by three numpy.NaN's.
 """
 
-from pysnptools.distreader.distreader import DistReader
-from pysnptools.distreader.distdata import DistData
-from pysnptools.distreader.distnpz import DistNpz
-from pysnptools.distreader.disthdf5 import DistHdf5
-from pysnptools.distreader.distmemmap import DistMemMap
-from pysnptools.distreader.bgen import Bgen
-from pysnptools.distreader.distgen import DistGen
-from pysnptools.distreader._distmergesids import _DistMergeSIDs
+from pysnptools.distreader.distreader import DistReader  # noqa: F401, E402
+from pysnptools.distreader.distdata import DistData  # noqa: F401, E402
+from pysnptools.distreader.distnpz import DistNpz  # noqa: F401, E402
+from pysnptools.distreader.disthdf5 import DistHdf5  # noqa: F401, E402
+from pysnptools.distreader.distmemmap import DistMemMap  # noqa: F401, E402
+from pysnptools.distreader.bgen import Bgen  # noqa: F401, E402
+from pysnptools.distreader.distgen import DistGen  # noqa: F401, E402
+from pysnptools.distreader._distmergesids import _DistMergeSIDs  # noqa: F401, E402
 
